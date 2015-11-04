eabde900e9d345535028ca4f5bb76ed522f58e9e
Palmer <pzpaul2002@yahoo.com>
2015-11-03 19:56:20 -0500

    code formatting

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-Slideshow
-=========
-
-Makes the background of your website into a slideshow. See this <a href="http://jsfiddle.net/pzp1997/5vTW6/26/embedded/result/">JSFiddle</a> of it in action!
+# Slideshow
+Makes the background of your website into a slideshow. See this [JSFiddle](http://jsfiddle.net/pzp1997/5vTW6/26/embedded/result/) of it in action!
--- Slideshow.js
+++ Slideshow.js
@@ -12,82 +12,82 @@ var displaySlider = true;
 var counter, timer;
 
 function update() {
-    if (displaySlider) {
-        $slider.css({
-            "background-image": "url(" + String(slides[counter]) + ")"
-        });
-        $transition.fadeOut(transitionTime);
-        $slider.fadeIn(transitionTime);
-    } else {
-        $transition.css({
-            "background-image": "url(" + String(slides[counter]) + ")"
-        });
-        $slider.fadeOut(transitionTime);
-        $transition.fadeIn(transitionTime);
-    }
+  if (displaySlider) {
+    $slider.css({
+      "background-image": "url(" + String(slides[counter]) + ")"
+    });
+    $transition.fadeOut(transitionTime);
+    $slider.fadeIn(transitionTime);
+  } else {
+    $transition.css({
+      "background-image": "url(" + String(slides[counter]) + ")"
+    });
+    $slider.fadeOut(transitionTime);
+    $transition.fadeIn(transitionTime);
+  }
 
-    displaySlider = !displaySlider;
-    counter++;
-    counter = counter % slides.length;
+  displaySlider = !displaySlider;
+  counter++;
+  counter = counter % slides.length;
 }
 
 function manualUpdate() {
-    clearInterval(timer);
-    update();
-    timer = setInterval(update, delay);
+  clearInterval(timer);
+  update();
+  timer = setInterval(update, delay);
 }
 
 function previous() {
-    if (counter === 0) {
-        if (slides.length < 2) {
-            counter = 0;
-        } else {
-            counter = slides.length - 2;
-        }
-    } else if (counter === 1) {
-        counter = slides.length - 1;
+  if (counter === 0) {
+    if (slides.length < 2) {
+      counter = 0;
     } else {
-        counter -= 2;
+      counter = slides.length - 2;
     }
-    manualUpdate();
+  } else if (counter === 1) {
+    counter = slides.length - 1;
+  } else {
+    counter -= 2;
+  }
+  manualUpdate();
 }
 
-$(document).ready(function () {
-    if (slides.length < 2) {
-        counter = 0;
-    } else {
-        counter = 1;
-    }
-    $transition.css({
-        "background-image": "url(" + String(slides[0]) + ")"
-    }).show();
+$(document).ready(function() {
+  if (slides.length < 2) {
+    counter = 0;
+  } else {
+    counter = 1;
+  }
+  $transition.css({
+    "background-image": "url(" + String(slides[0]) + ")"
+  }).show();
 
-    if (automatic) {
-        timer = setInterval(update, delay);
-    }
+  if (automatic) {
+    timer = setInterval(update, delay);
+  }
 
-    if (buttons) {
-        $("#back").click(function () {
-            previous();
-        });
-        $("#next").click(function () {
-            manualUpdate();
-        });
-    } else {
-        $("#back").hide();
-        $("#next").hide();
-    }
+  if (buttons) {
+    $("#back").click(function() {
+      previous();
+    });
+    $("#next").click(function() {
+      manualUpdate();
+    });
+  } else {
+    $("#back").hide();
+    $("#next").hide();
+  }
 
-    if (arrowKeys) {
-        $(document).keyup(function (event) {
-            switch (event.which) {
-                case 37:
-                    previous();
-                    break;
-                case 39:
-                    manualUpdate();
-                    break;
-            }
-        });
-    }
+  if (arrowKeys) {
+    $(document).keyup(function(event) {
+      switch (event.which) {
+        case 37:
+          previous();
+          break;
+        case 39:
+          manualUpdate();
+          break;
+      }
+    });
+  }
 });
